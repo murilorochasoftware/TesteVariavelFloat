8fc702f28d7dc7cf86dcb9de333bbc121688a109
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:45:46 -0300

    Teste do uso de variável com o tipo de dados float em Java.

---
+++ src/TesteVariavelFloat.java
@@ -0,0 +1,8 @@
+public class TesteVariavelFloat {
+
+    public static void main(String[] args) {
+        float seteDigitosDecimais = 0.1234567f;
+
+        System.out.printf("O valor com 7 digitos decimais é: %f", seteDigitosDecimais);
+    }
+}
